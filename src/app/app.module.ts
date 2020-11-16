52dbca07039dc3af95f6b9bb39d4831e72c59e3d
Rodrigo Rodrigues Souza <adm.isda@gmail.com>
2020-11-16 09:57:49 -0300

    primeiro commit do frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { ClientesService } from './clientes.service'
+import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientesModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
